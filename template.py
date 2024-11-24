7bfa0e4e633be91334df6d1c79a5023a9f6b9222
Hari krishna <tphari18@gmail.com>
2024-11-25 01:29:40 +0530

    spelling fix in template.py

--- template.py
+++ template.py
@@ -41,4 +41,4 @@ for filepath in List_of_files:
             logging.info(f"Creating empty file: {filepath}")
 
     else:
-        logging.info(f"{filename} is alredy exists")    
+        logging.info(f"{filename} is already exists")    
